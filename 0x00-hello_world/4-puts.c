b42e4f10cc114f49579b6a1ced499b4a8998b290
Greenie00 <georgegbalinyo@gmail.com>
2023-02-16 13:17:00 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 /**
  * A program that prints (Programming is like building a multilingual puzzle)
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
