b168cc52b1bb62523f90f9dd7294c30258502efe
Greenie00 <georgegbalinyo@gmail.com>
2023-02-17 18:43:59 -0800

    Print Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphab[26] = 'abcdefghijklmnopqrstuvwxyz';
+
+	int g = 0;
+
+	for (g < 26; i++)
+	{
+		putchar(alphab[1]);
+		putchar("\n")
+	}
+	return (0)
+}
