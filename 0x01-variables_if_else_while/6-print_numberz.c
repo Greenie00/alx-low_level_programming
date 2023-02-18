754e9e767c5de51dbffd0075af4432b194377dc5
Greenie00 <georgegbalinyo@gmail.com>
2023-02-17 19:58:23 -0800

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int g = 0;
+
+	while (g <= 10)
+	{
+		putchar(g + '0');
+		g++;
+	}
+	putchar('\n')
+	return (0)
+}	
