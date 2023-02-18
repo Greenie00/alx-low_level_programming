f6b26120e06dfe74a87235ccfc9753ea977d0ad3
Greenie00 <georgegbalinyo@gmail.com>
2023-02-17 19:10:15 -0800

    Print Alphabets CAP

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets capital
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char g = 'A';
+	char b = 'a';
+
+	while (b <= 'z')
+	{
+		putchar(b);
+		b++;
+	}
+	while (g <= 'Z')
+	{
+		putchar(g);
+		gtt;
+	}
+	putchar('\n')
+	return (0)
+}
