3bd378bab68e9f32929eeb9deedf68fb091bec88
Greenie00 <georgegbalinyo@gmail.com>
2023-02-21 14:02:23 -0800

    Lower case detector

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Returns 1 if small alphabet is found else returns 0
+ *
+ * Result: 0 and 1
+ */
+
+int _islower(int c)
+{
+
+	char alphab;
+
+	if (alphab = 'a' && alphab <= 'z')
+		return (1);
+	else
+		return (0);
+
+	return (0);
+}
