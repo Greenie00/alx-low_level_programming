6ec8663d7324225b01ed67e610165be6b978320f
Greenie00 <georgegbalinyo@gmail.com>
2023-02-21 07:56:22 -0800

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabets
+ *
+ * Result: return 0
+ */
+
+void main(void)
+{
+	char alphab;
+
+	for (alphab = 'a'; alphab <= 'c'; letter++)
+		_putchar(alphab);
+
+	_putchar('\n');
+}
