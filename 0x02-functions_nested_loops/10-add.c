025f80febb4ecad6cbc6d5715a600e760e46370a
Greenie00 <georgegbalinyo@gmail.com>
2023-02-21 18:16:11 -0800

    Add arguments

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add both arguments
+ * @g: first argument
+ * @h: Second argument 
+ *
+ * Return: 0
+ */
+
+int add(int g, int h)
+{
+	int i;
+
+	i = g + h;
+	return (i);
+}
