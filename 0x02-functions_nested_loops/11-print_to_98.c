0d9ce5f11e600024f40ca38980f6201c36dbd12e
Greenie00 <georgegbalinyo@gmail.com>
2023-02-21 19:11:22 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,37 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		printf("%d, ", n);
-		n++;
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d, ", n);
+				printf('\n');
+				break;
+			}
+
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
 	}
 
-	else if (n > 98)
+	else
 	{
-		printf("%d, ", n);
-		n--;
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d, ", n);
+				printf('\n');
+				break;
+			}
+
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
 	}
-	return (0);
 }
