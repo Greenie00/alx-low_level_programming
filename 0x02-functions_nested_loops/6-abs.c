292b73bcffc26b481e393ec413fabaa473c6789a
Greenie00 <georgegbalinyo@gmail.com>
2023-02-21 16:25:22 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value
+ *
+ * Return: Returns the absolute value 
+ */
+
+int _abs(int g)
+{
+	if (g < 0)
+	{
+		int absolute_val;
+		absolute_val = g * -1;
+		return (absolute_value);
+	}
+	return (g);
+}
