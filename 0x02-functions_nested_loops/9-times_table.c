500f85fd72602421c44f3befb46ea21106c99e2e
Greenie00 <georgegbalinyo@gmail.com>
2023-02-21 17:55:40 -0800

    timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Multiplication
+ *
+ * Return: 0 
+ */
+
+void times_table(void)
+{
+	if a, b, c, d, g, h;
+
+	for(x = 0; a <= 9; a++)
+	{
+		for(b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				g = c % 10;
+				h = (c -g) / 10;
+				_putchar(g + '0');
+				_putchar(h + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
