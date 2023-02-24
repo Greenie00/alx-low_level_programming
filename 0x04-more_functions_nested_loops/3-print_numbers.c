3724f42ae5992a3228c3d5fce200e4b33149d8c4
Greenie00 <georgegbalinyo@gmail.com>
2023-02-24 07:00:44 -0800

    Count up

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: 0-9
+ */
+
+void print_numbers(void)
+{
+	int g = 0;
+
+	while (g < 10)
+	{
+		g++;
+		_putchar("%d");
+	}
+	_putchar("\n");
+}
