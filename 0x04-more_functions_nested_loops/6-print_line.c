fcdf7958bcdd41988d1b8600e6fee4af49bcd167
Greenie00 <georgegbalinyo@gmail.com>
2023-02-24 13:22:23 -0800

    Up

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - Prints underscore to form a line
+ *
+ * @n: Argument
+ * Return: Lines
+ */
+
+void print_line(int n)
+{
+	int g;
+
+	if (g <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (g = 0; g < b; g++)
+		{
+			_putchar(95);
+		}
+	_putchar('\n');
+	}
+}
