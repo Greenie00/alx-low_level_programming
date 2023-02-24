1d0144e6a3fbeca448eae008b0b72cda981a098e
Greenie00 <georgegbalinyo@gmail.com>
2023-02-24 15:02:31 -0800

    Tr33

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ *
+ * @size: argument
+ * Return: A square
+ */
+
+void print_square(int size)
+{
+	int x,y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (x = 9; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar("#");
+			
+			}
+		_putchar('\n');
+		}
+	}
+}
