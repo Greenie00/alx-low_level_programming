69012e9fe6513ba0c0681d001028c6bddd714743
Greenie00 <georgegbalinyo@gmail.com>
2023-02-24 06:52:23 -0800

    Multiply given arguments

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies the given arguments
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: Answer of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
