32dab9e482478fc8fcb8672cf1f6666f3e892d3b
Greenie00 <georgegbalinyo@gmail.com>
2023-02-24 06:43:57 -0800

    Is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -11,7 +11,16 @@ int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
 	{
-	return (1);
+		return (1);
 	}
-	return (0);
+
+	else if (c < 0 || c > 9)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (0);
+	}
 }
