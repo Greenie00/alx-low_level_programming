f559c22d15fafb40ba5db86ae4aa7cf56a806574
Greenie00 <georgegbalinyo@gmail.com>
2023-02-27 07:24:37 -0800

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Retuens string length of character
+ *
+ * @s: Argument
+ * Return: Length of string 
+ */
+
+int _strlen(char *s)
+{
+	int length_of_s = 0;
+
+	while (*s++)
+	{
+		length_of_s++;
+	}
+	return(length_of_s);
+}
