a50e8b851982d9687c3115f1cce48b4f595d0541
Greenie00 <georgegbalinyo@gmail.com>
2023-02-27 16:59:16 -0800

    Complicated

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#incude "main.h"
+
+/**
+ * _atoi - Converts strings to integers 
+ *
+ * @s: Argument
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+	int c;
+	unsigned int n = 0;
+	int min = 1;
+	int l = 0;
+
+	while (s[c] == 45)
+	{
+		min *= -1;
+	}
+
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+		l = 1;
+		n = (n * 10) + (s[c] - '0');
+		c++;
+	}
+
+	if (l == 1)
+	{
+		break;
+	}
+	c++;
+}
