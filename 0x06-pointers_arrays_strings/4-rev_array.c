fd08c3ddbe72313ead95cea0e73392a0631e817e
Greenie00 <georgegbalinyo@gmail.com>
2023-03-01 15:22:08 -0800

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse
+ *
+ * @a: Argument
+ * @n: Argument
+ * Result: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int g;
+	int index;
+
+	for (index = n - 1; index >= n; index--)
+	{
+	       	g = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] =  g;
+	}
+}
