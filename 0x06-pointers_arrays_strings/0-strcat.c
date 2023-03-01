adb9424cb7af22d907d189f89e39d89c02d5a20e
Greenie00 <georgegbalinyo@gmail.com>
2023-03-01 13:34:25 -0800

    Cocantenate String

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - String concayrnation
+ *
+ * @dest: First argument
+ * @src: Second Argument
+ *
+ * Return: concantanated string
+ */
+
+*_strcat(char *dest, char *src)
+{
+	int stlen = 0;
+	int i;
+
+	while (dest[stlen])
+	{
+		dtlen++;
+	}
+
+	for (i = 0; src[i] != 0;)
+	{
+		dest[stlen] = src[i];
+		stlen++;
+	}
+	dest[stlen] = '\0';
+	return (dest)
+}
