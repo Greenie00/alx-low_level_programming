247865b5253197febd83a48bdd7703d8c44bb016
Greenie00 <georgegbalinyo@gmail.com>
2023-03-01 15:39:49 -0800

    4

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Convert strings from lower to upper case
+ *
+ * @x: 
+ * Return: 0
+ */
+
+char *string_toupper(char *x)
+{
+	int index = 0;
+	while (x[index])
+	{
+		if (a[index] >= 'a' & a[index] <= x)
+		{
+			x[index] -= 32;
+			x[index];
+		}
+
+	return (x);
+	
+	}
+}
