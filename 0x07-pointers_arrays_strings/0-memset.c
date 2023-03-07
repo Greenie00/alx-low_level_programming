e046aa0e03a43d9bf5571c1377acc6a51e404b64
Greenie00 <georgegbalinyo@gmail.com>
2023-03-06 21:24:14 -0800

    Memeset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - Creating  a replica 
+ *
+ * @s: First argument
+ * @b: Seconf argument
+ * @n: third argument
+ *
+ * Result: Copies contents of b into s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < 0)
+	{
+		s[i] = b;
+		i++;
+	}
+	reeturn (S);
+}
