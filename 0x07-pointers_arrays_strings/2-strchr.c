115dc0b145c51e3d9af40af6f7c32b92eeb228cc
Greenie00 <georgegbalinyo@gmail.com>
2023-03-07 01:55:58 -0800

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Locating a character 
+ *
+ * @s: First argument
+ * @c: Second argument
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+			return (s + 1);
+		i++;
+	}
+
+	return (NULL);
+}
