ba7d4054e53f3df80dd54e1e0b4efcb3bad87ea9
Greenie00 <georgegbalinyo@gmail.com>
2023-03-07 16:05:17 -0800

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int x = 0;
-	int y = 0;
+	int x;
+	int y;
 
-	for (; x < 8; x++)
+	for (x = 0; x < 8; x++)
 	{
-		for  (; y < 8; y++)
+		for  (x = 0; y < 8; y++)
 			_putchar(a[x][y]);
 		_putchar('\n');
 	}
