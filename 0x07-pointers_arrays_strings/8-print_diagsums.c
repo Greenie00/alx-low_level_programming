1e695610637e15c6adf7a62314b33d32e06377ba
Greenie00 <georgegbalinyo@gmail.com>
2023-03-07 16:57:38 -0800

    Add diagonally

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,13 +17,9 @@ void print_diagsums(int *a, int size)
 	int sum2 = 0;
 
 	for (x = 0; x <= (size * size); x = x + size + 1)
-	{
 		sum1 = sum1 + a[x];
-	}
 
 	for (y = size - 1; y <= (size * size) - size; y = y + size - 1)
-	{
-		sum2 = sum2 + a[y];
-		printf("%d, %d\n", sum1, sum2);
-	}
+	sum2 = sum2 + a[y];
+	printf("%d, %d\n", sum1, sum2);
 }
