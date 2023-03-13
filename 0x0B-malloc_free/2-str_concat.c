a38cde4d377793b4b7468b4ba88aa05d6642afa4
Greenie00 <georgegbalinyo@gmail.com>
2023-03-13 12:04:05 -0700

    MAlloc 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,10 +4,8 @@
 
 /**
  * str_concat - String concantenation
- *
  * @s1: First argument
  * @s2: Second argument
- *
  * Return: always 0
  */
 
@@ -18,41 +16,28 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[i])
-	{
 		i++;
-	}
 
 	while (s2[j])
-	{
 		j++;
-	}
 
 	l = i + j;
-
 	s = malloc((sizeof(char) * l) + 1);
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
-
+	j = 0;
 	while (k < l)
 	{
 		if (k <= i)
-		{
 			s[k] = s1[k];
-		}
 
 		if (k >= i)
 		{
@@ -61,7 +46,6 @@ char *str_concat(char *s1, char *s2)
 		}
 		k++;
 	}
-
 	s[k] = '\0';
 	return (s);
 }
