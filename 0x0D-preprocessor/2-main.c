ec0fc134059a9f2c543a7d586378ab494f38ee85
Greenie00 <georgegbalinyo@gmail.com>
2023-03-20 15:25:45 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function for returning file name
+ *
+ * Return: File name
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
