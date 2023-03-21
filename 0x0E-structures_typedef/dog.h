b6efa632fc5ee72d2e9e1c865f7b3c2d569c5549
Greenie00 <georgegbalinyo@gmail.com>
2023-03-21 12:42:46 -0700

    !

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Dog attributes
+ * @name: First argument
+ * @age: Age 
+ * @owner: Name
+ */
+
 struct dog
 {
 	char *name;
@@ -8,6 +15,9 @@ struct dog
 	char *owner;
 }
 
+/**
+ * dog_t - Typedef for dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
