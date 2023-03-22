9788391f419f69ecb3f007c45a55281d8d7ba24b
Greenie00 <georgegbalinyo@gmail.com>
2023-03-22 14:30:56 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (x < size)
+			{
+				if (cmp(array[x]))
+					return (x);
+				x++;
+			}
+		}
+	}
+
+	return (-1)
+}
