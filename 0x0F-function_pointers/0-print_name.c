982af9f878a73ee8a6ee4ead28dc0dd8fa7f825f
Greenie00 <georgegbalinyo@gmail.com>
2023-03-22 14:06:46 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,14 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - Prints name
+ *
+ * @name: Arg
+ * @f: arg
+ *
+ * Return: NAme
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
