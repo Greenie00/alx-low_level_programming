e2cdd8ba0a5519e1534d67c2e20bcdc946f8f159
Greenie00 <georgegbalinyo@gmail.com>
2023-04-11 16:30:45 -0700

    Fix

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * (8))
+		return (-1);
+
+	ptr = *n;
+
+	ptr ^= (1 << index);
+	return (1);
+}
